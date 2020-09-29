25c79a5eb924cdb40f0a5b1027c7b5b42c39e6f9
Cameron Siggins <sigginscameron@gmail.com>
2020-09-29 18:05:53 -0500

    Added save() to scenefile.py
    scenefile.py can now save files to a path and creates new directories if any are missing from the path

--- src/scenefile.py
+++ src/scenefile.py
@@ -1,8 +1,13 @@
+import logging
+
+import pymel.core as pmc
 from pymel.core.system import Path
 
+log = logging.getLogger(__name__)
 
 class SceneFile(object):
     """Abstract representation of a Scene file."""
+
     def __init__(self, path):
         self.folder_path = Path()
         self.descriptor = 'main'
@@ -30,12 +35,15 @@ class SceneFile(object):
         self.descriptor, self.task, ver = path.name.stripext().split("_")
         self.ver = int(ver.split("v")[-1])
 
+    def save(self):
+        """Saves the scene file.
 
-scene_file = SceneFile("D:/sandbox/tank_model_v001.ma")
-print(scene_file.folder_path)
-print(scene_file.descriptor)
-print(scene_file.task)
-print(scene_file.ver)
-print(scene_file.ext)
-print(scene_file.filename)
-print(scene_file.path)
+        Returns:
+            Path: The path to the scene file if successful
+        """
+        try:
+            return pmc.system.saveAs(self.path)
+        except RuntimeError as err:
+            log.warning("Missing directories in path. Creating directories...")
+            self.folder_path.makedirs_p()
+            return pmc.system.saveAs(self.path)
