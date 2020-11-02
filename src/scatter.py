ef3bd10f07e6b4e2c2710b0346a395fb36d91887
Cameron Siggins <sigginscameron@gmail.com>
2020-11-01 19:12:16 -0600

    Scatter Button Added
    Added UI Button for Scatter

--- src/scatter.py
+++ src/scatter.py
@@ -21,9 +21,36 @@ class ScatterToolUI(QtWidgets.QDialog):
 
     def __init__(self):
         super(ScatterToolUI, self).__init__(parent=maya_main_window())
-        self.setWindowTitle("Scatter")
+        self.setWindowTitle("Scatter Tool")
         self.setMinimumWidth(500)
         self.setMaximumHeight(200)
+        self.scatter_tool = ScatterTool()
+        self.create_ui()
+        self.create_connections()
+
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("Scatter Objects")
+        self.title_lbl.setStyleSheet("font: bold 20px")
+        self.button_lay = self._create_button_ui()
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addLayout(self.button_lay)
+        self.setLayout(self.main_lay)
+
+    def create_connections(self):
+        """Connect Signals and Slots"""
+        self.scatter_btn.clicked.connect(self._scatter)
+
+    @QtCore.Slot()
+    def _scatter(self):
+        """Scatter objects"""
+        # self._set_scenefile_properties_from_ui()
+        self.scatter_tool.scatter()
+
+    def _create_button_ui(self):
+        self.scatter_btn = QtWidgets.QPushButton("Scatter")
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.scatter_btn)
+        return layout
 
 
 class ScatterTool(object):
@@ -55,14 +82,14 @@ class ScatterTool(object):
 
     @staticmethod
     def random_rotation(new_instance):
-        min_rotate_x = 10.0
-        max_rotate_x = 90.0
-        rand_rotation_x = rand.uniform(min_rotate_x, max_rotate_x)
-        min_rotate_y = 10.0
-        max_rotate_y = 90.0
-        rand_rotation_y = rand.uniform(min_rotate_y, max_rotate_y)
-        min_rotate_z = 10.0
         max_rotate_z = 90.0
+        min_rotate_z = 10.0
+        max_rotate_y = 90.0
+        min_rotate_y = 10.0
+        max_rotate_x = 90.0
+        min_rotate_x = 10.0
+        rand_rotation_x = rand.uniform(min_rotate_x, max_rotate_x)
+        rand_rotation_y = rand.uniform(min_rotate_y, max_rotate_y)
         rand_rotation_z = rand.uniform(min_rotate_z, max_rotate_z)
         cmds.rotate(rand_rotation_x, rand_rotation_y, rand_rotation_z,
                     new_instance, r=True)
