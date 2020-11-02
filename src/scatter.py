54dadcfadcbf376f071da97e108453c39caf89cd
Cameron Siggins <sigginscameron@gmail.com>
2020-11-01 20:29:32 -0600

    Added Random Scale Modifier UI
    Added UI for the min and max values of the random scale method

--- src/scatter.py
+++ src/scatter.py
@@ -22,7 +22,7 @@ class ScatterToolUI(QtWidgets.QDialog):
     def __init__(self):
         super(ScatterToolUI, self).__init__(parent=maya_main_window())
         self.setWindowTitle("Scatter Tool")
-        self.setMinimumWidth(500)
+        self.setMinimumWidth(300)
         self.setMaximumHeight(200)
         self.scatter_tool = ScatterTool()
         self.create_ui()
@@ -31,8 +31,11 @@ class ScatterToolUI(QtWidgets.QDialog):
     def create_ui(self):
         self.title_lbl = QtWidgets.QLabel("Scatter Objects")
         self.title_lbl.setStyleSheet("font: bold 20px")
+        self.scale_lay = self._create_max_scale_ui()
         self.button_lay = self._create_button_ui()
         self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.main_lay.addLayout(self.scale_lay)
         self.main_lay.addLayout(self.button_lay)
         self.setLayout(self.main_lay)
 
@@ -43,8 +46,12 @@ class ScatterToolUI(QtWidgets.QDialog):
     @QtCore.Slot()
     def _scatter(self):
         """Scatter objects"""
-        # self._set_scenefile_properties_from_ui()
+        self._set_scatter_properties_from_ui()
         self.scatter_tool.scatter()
+
+    def _set_scatter_properties_from_ui(self):
+        self.scatter_tool.min_scale = self.min_scale_sbx.value()
+        self.scatter_tool.max_scale = self.max_scale_sbx.value()
 
     def _create_button_ui(self):
         self.scatter_btn = QtWidgets.QPushButton("Scatter")
@@ -52,10 +59,50 @@ class ScatterToolUI(QtWidgets.QDialog):
         layout.addWidget(self.scatter_btn)
         return layout
 
+    def _create_min_scale_ui(self):
+        layout = self._create_modifier_headers()
+        self.min_scale_sbx = QtWidgets.QDoubleSpinBox()
+        self.min_scale_sbx.setSingleStep(0.1)
+        self.min_scale_sbx.\
+            setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.min_scale_sbx.setFixedWidth(100)
+        self.min_scale_sbx.setValue(self.scatter_tool.min_scale)
+        layout.addWidget(QtWidgets.QLabel("Minimum Scale:"), 1, 0)
+        layout.addWidget(self.min_scale_sbx, 1, 1)
+        return layout
+
+    def _create_max_scale_ui(self):
+        layout = self._create_min_scale_ui()
+        self.max_scale_sbx = QtWidgets.QDoubleSpinBox()
+        self.max_scale_sbx.setSingleStep(0.1)
+        self.max_scale_sbx. \
+            setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.max_scale_sbx.setFixedWidth(100)
+        self.max_scale_sbx.setValue(self.scatter_tool.max_scale)
+        layout.addWidget(QtWidgets.QLabel("Maximum Scale:"), 2, 0)
+        layout.addWidget(self.max_scale_sbx, 2, 1)
+        return layout
+
+    def _create_modifier_headers(self):
+        self.scale_header_lbl = QtWidgets.QLabel("Random Scale Modifier")
+        self.scale_header_lbl.setStyleSheet("font: bold 15px")
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.scale_header_lbl, 0, 0)
+        return layout
+
 
 class ScatterTool(object):
 
-    @QtCore.Slot()
+    def __init__(self):
+        self.max_rotate_z = 90.0
+        self.min_rotate_z = 10.0
+        self.max_rotate_y = 90.0
+        self.min_rotate_y = 10.0
+        self.max_rotate_x = 90.0
+        self.min_rotate_x = 10.0
+        self.min_scale = 1.0
+        self.max_scale = 1.0
+
     def scatter(self):
         """Scatter object along the vertices of another object"""
         selection = cmds.ls(os=True, fl=True)
@@ -72,24 +119,14 @@ class ScatterTool(object):
         else:
             print("Please ensure the object you select is a transform")
 
-    @staticmethod
-    def random_scale(new_instance):
-        min_scale = 0.8
-        max_scale = 1.2
-        rand_scale = rand.uniform(min_scale, max_scale)
+    def random_scale(self, new_instance):
+        rand_scale = rand.uniform(self.min_scale, self.max_scale)
         cmds.scale(rand_scale, rand_scale, rand_scale, new_instance,
                    r=True)
 
-    @staticmethod
-    def random_rotation(new_instance):
-        max_rotate_z = 90.0
-        min_rotate_z = 10.0
-        max_rotate_y = 90.0
-        min_rotate_y = 10.0
-        max_rotate_x = 90.0
-        min_rotate_x = 10.0
-        rand_rotation_x = rand.uniform(min_rotate_x, max_rotate_x)
-        rand_rotation_y = rand.uniform(min_rotate_y, max_rotate_y)
-        rand_rotation_z = rand.uniform(min_rotate_z, max_rotate_z)
+    def random_rotation(self, new_instance):
+        rand_rotation_x = rand.uniform(self.min_rotate_x, self.max_rotate_x)
+        rand_rotation_y = rand.uniform(self.min_rotate_y, self.max_rotate_y)
+        rand_rotation_z = rand.uniform(self.min_rotate_z, self.max_rotate_z)
         cmds.rotate(rand_rotation_x, rand_rotation_y, rand_rotation_z,
                     new_instance, r=True)
